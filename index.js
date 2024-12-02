d1ef7b71ca39853ff3d7795060b83cf437b3e1e8
Eloool <csm.bolon@free.fr>
2024-12-02 10:59:32 +0100

    Ajout de pages Handlebars

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
+//Ajout des modules dont on a besoins
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
+const hbs = require("hbs");
+const path = require("path");
 
 const app = express();
 const prisma = new PrismaClient();
 const PORT = 3008;
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "WebPages")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "WebPages", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
 
 // On définit un middleware pour parser les données des requêtes entrantes.
 // Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/tasks", async (req, res) => {
-    const tasks = await prisma.task.findMany();
-    res.json(tasks);
+const gamesGenres = ["Action","Aventure","RPG","Simulation","Sport","MMORPG"];
+
+app.get("/", async (req, res) => {
+    res.render("index");
 });
 
-app.post("/tasks", async (req, res) => {
-    const { title } = req.body;
-    try {
-        await prisma.task.create({
-            data: { title },
-        }); // Ici on ne stock pas le retour de la requête, mais on attend quand même son exécution
-        res.status(201).redirect("/tasks"); // On redirige vers la page des tâches
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ error: "Task creation failed" });
-    }
+app.get("/games", async (req, res) => {
+    const games = await prisma.games.findMany();
+    res.render("games", {
+        games,
+    });
 });
 
 app.listen(PORT, () => {
